9744d858ee9063d4df46528cd7a0dda9ca0c2b83
qhur135 <qhur135@gmail.com>
2021-08-30 03:10:18 +0900

    test

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerInfo : MonoBehaviourPunCallbacks
+{
+    //[SerializeField] TMP_Text playeridtext; 
+
+    const string UPDATE_Player_ID = "UpdatePlayerID_RPC";
+    PhotonView pv;
+
+    public string PlayerID { get; set; }
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+    [PunRPC]
+    public void UpdatePlayerID_RPC(string id)
+    {
+        PlayerID = id;
+        //playeridtext.text = PlayerID;
+    }
+    public void SetPlayerID(string id)
+    {
+        pv.RPC(UPDATE_Player_ID, RpcTarget.All, id);
+    }
+    public string getplayerid()
+    {
+        return PlayerID;
+    }
+    //public override void OnPlayerEnteredRoom(Player newPlayer)
+    //{
+    //    if (!pv.IsMine) return;
+    //    base.OnPlayerEnteredRoom(newPlayer);
+    //    pv.RPC(UPDATE_Player_ID, RpcTarget.All, PlayerID);
+    //}
+}
--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Runner : Player 
+public class Runner : Player
 {
 
     const string START_LINE_TAG = "StartLine";
@@ -13,18 +13,21 @@ public class Runner : Player
 
     Vector3 lastPosition;
     bool godmode; // 무적 상태 - 움직여도 안걸림 
-    bool passStartLine; 
+    bool passStartLine;
     bool isCaught;
-  
 
+    PlayerInfo playerinfo;
 
     public override void Awake()
     {
         base.Awake();
-        godmode =true; 
-        passStartLine = false; 
+        godmode = true;
+        passStartLine = false;
         isCaught = false;
         print("Runner Awake");
+
+
+        playerinfo = GetComponent<PlayerInfo>();
     }
     //public override void Start()
     //{
@@ -38,7 +41,7 @@ public class Runner : Player
     }
     public override void Update()
     {
-        if (!PV.IsMine) return;
+        //if (!PV.IsMine) return;
         base.Update();
 
         if (passStartLine)
@@ -50,10 +53,10 @@ public class Runner : Player
             godmode = true;
         }
 
-       
+
         if (flowerMsgController.isFlowerEnd())
-        { 
-            if (!isCaught && !godmode && lastPosition != transform.position) 
+        {
+            if (!isCaught && !godmode && lastPosition != transform.position)
             {
                 isCaught = true;
                 print("딱걸렸어!");
@@ -62,8 +65,8 @@ public class Runner : Player
         }
         else // 멘트 다 외치기 전에 계속해서 최근 위치 업데이트
         {
-             lastPosition = transform.position;
-             //print("position update");   
+            lastPosition = transform.position;
+            //print("position update");   
         }
 
     }
